c3d37f231bd9496de117d825bb7a3ae7b1cdc7f1
yareach <mine8345@naver.com>
2026-04-14 15:53:26 +0900

    comment - 1000, 1001번 문제 주석 추가

--- bj1000.asm
+++ bj1000.asm
@@ -12,6 +12,8 @@ output_size equ $ - output
 section .text
 global main
 main:
+    ;-- 입력 --
+    ; input에 입력받은 값 저장
     mov rax, SYS_READ
     mov rdi, STDIN
     lea rsi, [rel input]
@@ -23,7 +25,9 @@ main:
     mov ah, byte [rel input + 2]
     sub ah, '0'
 
+    ; 덧셈
     add al, ah
+    ; 이후 나누기 작업을 위해 0으로 설정
     xor ah, ah
 
     ;al = al % 10, ah = ah % 10
@@ -31,6 +35,7 @@ main:
     div cl
 
     ; fill output
+    ; rcx: 문자수, rbx: 문자열의 시작
     mov rcx, 0
     lea rbx, [rel output]
     add rbx, output_size - 1
@@ -42,13 +47,14 @@ main:
     cmp al, 0
     je print
 
+    ; 10이상일 경우 10의 자리 추가
     dec rbx
     inc rcx
     add al, '0'
     add [rbx], al
 
-    ; print
 print:
+    ; print
     mov rax, SYS_WRITE
     mov rdi, STDOUT
     mov rsi, rbx
--- bj1001.asm
+++ bj1001.asm
@@ -12,49 +12,59 @@ output_size equ $ - output
 section .text
 global main
 main:
+    ; 1. ----- 입력받기 -----
+    ; input에 입력된 값 저장
     mov rax, SYS_READ
     mov rdi, STDIN
     lea rsi, [rel input]
     mov rdx, 3
     syscall
 
+    ; 2. ----- 파싱 -----
+    ;al, ah에 숫자 저장
     mov al, byte [rel input]
-    sub al, '0'
     mov ah, byte [rel input + 2]
+    ;ascii값에서 숫자로 변환
+    sub al, '0'
     sub ah, '0'
 
-    mov cl, 0 ; cl == 0 means result is positive
+    ; 3. ----- 계산 -----
+    ; 절댓값을 구하고 cl값에 따라 '-' 기호를 문자열에 추가
+    ; cl은 부호플래그 0일시 양수, 1일시 음수
+    mov cl, 0 ;초기화
     cmp al, ah
     jge comp
     mov cl, al
     mov al, ah
     mov ah, cl
-    mov cl, 1 ; cl == 1 means result is negative
+    mov cl, 1 ;음수
 comp:
+    ; al에 연산의 절댓값 저장
     sub al, ah
 
-    ; fill output
-    mov rdx, 0
-    lea rbx, [rel output]
-    add rbx, output_size - 1
+    ; 4. ----- 문자열 제작 -----
+    mov rdx, 0                  ; rdx : 문자열 길이
+    lea rbx, [rel output]       ; rbx : 문자열 시작점
+    add rbx, output_size - 1    ; rbx를 output의 마지막으로 초기화
 
     inc rdx
-    add al, '0'
+    add al, '0'                 ; 숫자에서 ascii 값으로
     mov [rbx], al
 
-    cmp cl, 0
-    je print
+    cmp cl, 0                   ; 부호 플래그가 0인경우(양수인 경우)
+    je print                    ; 출력으로 점프
 
+    ; 음수일 경우 '-'기호 추가
     dec rbx
     inc rdx
     add [rbx], byte '-'
 
-    ; print
 print:
+    ; 5. ----- 출력 -----
+    ; rdx는 문자열을 제작하며 할당하였기에 여기서 따로 할당하지는 않음
     mov rax, SYS_WRITE
     mov rdi, STDOUT
     mov rsi, rbx
-
     syscall
 
     xor rax, rax
